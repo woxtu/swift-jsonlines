2f9348730314cfa637f71716f6608cf700eff890
woxtu <woxtup@gmail.com>
2023-01-24 19:31:38 +0900

    Decode values asynchronously

--- Sources/NDJSON/NDJSON.swift
+++ Sources/NDJSON/NDJSON.swift
@@ -1,6 +1,13 @@
 import Foundation
 
+public enum StreamError: Error {
+  case unavailable(Error?)
+  case noBytesAvailable
+}
+
 public class NDJSONDecoder {
+  let readBufferSize: Int = 1024
+
   public init() {}
 
   public func decode<T>(_ type: T.Type, from data: Data) throws -> [T] where T: Decodable {
@@ -9,6 +16,65 @@ public class NDJSONDecoder {
     return try data
       .split(separator: .newline)
       .map { try decoder.decode(type, from: $0) }
+  }
+
+  @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0,*)
+  public func stream<T>(_ type: T.Type, from stream: InputStream) -> AsyncThrowingStream<T, Error> where T: Decodable {
+    let decoder = JSONDecoder()
+
+    return AsyncThrowingStream { continuation in
+      if stream.streamStatus == .notOpen {
+        stream.open()
+      }
+
+      guard stream.streamStatus == .open else {
+        continuation.finish(throwing: StreamError.unavailable(stream.streamError))
+        return
+      }
+
+      continuation.onTermination = { _ in
+        stream.close()
+      }
+
+      var data = Data()
+
+      while !data.isEmpty || stream.hasBytesAvailable {
+        if let index = data.firstIndex(of: .newline) {
+          do {
+            continuation.yield(try decoder.decode(type, from: data[..<index]))
+            data.removeSubrange(...index)
+            continue
+          } catch {
+            continuation.finish(throwing: error)
+            return
+          }
+        }
+
+        if stream.hasBytesAvailable {
+          let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: readBufferSize)
+          let readSize = stream.read(buffer, maxLength: readBufferSize)
+          if readSize > 0 {
+            data.append(buffer, count: readSize)
+            continue
+          } else {
+            continuation.finish(throwing: StreamError.noBytesAvailable)
+            return
+          }
+        }
+
+        break
+      }
+
+      do {
+        continuation.yield(try decoder.decode(type, from: data))
+        data.removeAll()
+      } catch {
+        continuation.finish(throwing: error)
+        return
+      }
+
+      continuation.finish()
+    }
   }
 }
 
--- Tests/NDJSONTests/NDJSONTests.swift
+++ Tests/NDJSONTests/NDJSONTests.swift
@@ -14,4 +14,15 @@ final class NDJSONDecoderTests: XCTestCase {
       [0, 1, 2]
     )
   }
+
+  func testDecodeValuesAsynchronously() async throws {
+    let decoder = NDJSONDecoder()
+    let data = """
+      0
+      1
+      2
+    """.data(using: .utf8)!
+    let result = try await decoder.stream(Int.self, from: InputStream(data: data)).reduce(into: []) { $0.append($1) }
+    XCTAssertEqual(result, [0, 1, 2])
+  }
 }
