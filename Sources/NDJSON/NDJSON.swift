623814c90cf1b40cad47b20a36333ed4b85c20be
woxtu <woxtup@gmail.com>
2023-01-24 19:20:12 +0900

    Decode values

--- Sources/NDJSON/NDJSON.swift
+++ Sources/NDJSON/NDJSON.swift
@@ -1,5 +1,17 @@
-public struct NDJSON {
-  public private(set) var text = "Hello, World!"
+import Foundation
 
+public class NDJSONDecoder {
   public init() {}
+
+  public func decode<T>(_ type: T.Type, from data: Data) throws -> [T] where T: Decodable {
+    let decoder = JSONDecoder()
+
+    return try data
+      .split(separator: .newline)
+      .map { try decoder.decode(type, from: $0) }
+  }
+}
+
+extension UInt8 {
+  static let newline: UInt8 = 0x0A
 }
--- Tests/NDJSONTests/NDJSONTests.swift
+++ Tests/NDJSONTests/NDJSONTests.swift
@@ -1,8 +1,17 @@
 @testable import NDJSON
 import XCTest
 
-final class NDJSONTests: XCTestCase {
-  func testExample() throws {
-    XCTAssertEqual(NDJSON().text, "Hello, World!")
+final class NDJSONDecoderTests: XCTestCase {
+  func testDecodeValues() throws {
+    let decoder = NDJSONDecoder()
+    let data = """
+      0
+      1
+      2
+    """.data(using: .utf8)!
+    XCTAssertEqual(
+      try decoder.decode(Int.self, from: data),
+      [0, 1, 2]
+    )
   }
 }
